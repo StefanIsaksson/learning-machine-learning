8450b51453443f0107185a65e62d4bba7f96eff2
Stefan Isaksson <stefan.foo.isaksson@gmail.com>
2019-08-03 22:53:45 +0200

    WIP - script which uses model to predict male or female

--- people/README.md
+++ people/README.md
@@ -12,6 +12,22 @@ Create a new conda environment  with Tensorflow.
 conda create -n keras_tf_env pillow tensorflow keras
 ```
 
+## Usage:
+
+Creates a model file called: `male_vs_female_model_WITHOUT_using_transfer_learning.h5`
+```
+python create_model_without_transfer_learning.py
+```
+
+Read input file and prints prediction if picture is male or female.
+```
+python predict_female_vs_male.py "example_input/male.png"
+```
 
 
+## Measured Model Accuracy
 
+|Model                                           | Accuracy |
+|------------------------------------------------|----------|
+|Transfer learning model stats (2 epochs only ..)|0.8522    |
+|No Transfer learning model stats (10 epochs)    |0.8130    |
---
+++ people/predict_female_vs_male.py
@@ -0,0 +1,41 @@
+import argparse
+from sys import argv
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+
+
+def predict_gender(input_file):
+    model = load_model('male_vs_female_model_WITHOUT_using_transfer_learning.h5')
+
+    img_width, img_height = 150, 150
+    img = image.load_img(input_file, target_size=(img_width, img_height))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+
+    images = np.vstack([x])
+    classes = model.predict_classes(images, batch_size=10)
+
+    prediction_map = {
+        0 : 'female',
+        1 : 'male'
+    }
+
+    gender_prediction = prediction_map[classes[0]]
+    return gender_prediction
+
+
+def main(input_file=None):
+    prediction = predict_gender(input_file)
+    print(f'"{input_file}" predicted to be {prediction}')
+    return prediction
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description='Classifies human portrait as either male or female)')
+    parser.add_argument('input_file', help='input file, e.g. ./example_input/female.png')
+    return parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    main(**parse_args(argv[1:]).__dict__)
